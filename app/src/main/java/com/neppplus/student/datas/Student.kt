6b5226923901c1369c9cd2a5ef6e893c60c5b36c
홍준호 <0615wnsgh@gmail.com>
2021-03-20 12:47:40 +0900

    함수 추가 예제

--- app/src/main/java/com/neppplus/student/datas/Student.kt
+++ app/src/main/java/com/neppplus/student/datas/Student.kt
@@ -1,7 +1,26 @@
 package com.neppplus.student.datas
 
+import java.util.*
+
 class Student(
     val name: String,
     val birthYear: Int,
     val address: String)  {
+
+    fun getKoreanAge() : Int {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+        val koreanAge = currentYear - this.birthYear + 1
+
+        return koreanAge
+    }
+
+
+    fun addTwoNumbers(num1 : Int, num2 : Int) : Int {
+
+        val result = num1 + num2
+
+        return result
+    }
+
 }
