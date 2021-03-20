c4414c0193e58c0beacc2619484c0dc3ae14b52c
홍준호 <0615wnsgh@gmail.com>
2021-03-20 12:17:10 +0900

    경고창 띄우기 예제

--- app/src/main/java/com/neppplus/student/MainActivity.kt
+++ app/src/main/java/com/neppplus/student/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.neppplus.student
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.neppplus.student.adapters.StudentAdapter
 import com.neppplus.student.datas.Student
 import kotlinx.android.synthetic.main.activity_main.*
@@ -35,8 +37,17 @@ class MainActivity : AppCompatActivity() {
 
         studentListView.setOnItemLongClickListener { parent, view, position, id ->
 
-            mStudentList.removeAt(position)
-            mAdapter.notifyDataSetChanged()
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("학생 삭제")
+            alert.setMessage("정말 학생을 삭제하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                mStudentList.removeAt(position)
+                mAdapter.notifyDataSetChanged()
+            })
+            alert.setNegativeButton("취소", null)
+
+            alert.show()
+
 
             return@setOnItemLongClickListener true
         }
